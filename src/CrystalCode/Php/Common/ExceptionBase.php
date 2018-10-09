994c15d4075594492d96c72d28307f98f83dd2b3
Dan Lugg <dan.lugg@gmail.com>
2018-10-09 00:33:47 -0400

    Refactorted to use inheritdoc on derivations. Cleaned up some use references.

--- src/CrystalCode/Php/Common/ExceptionBase.php
+++ src/CrystalCode/Php/Common/ExceptionBase.php
@@ -2,9 +2,20 @@
 
 namespace CrystalCode\Php\Common;
 
-use \Exception;
+use Exception;
 
 abstract class ExceptionBase extends Exception
 {
-    
+
+    /**
+     * 
+     * @param string $message
+     * @param int $code
+     * @param Exception $previous
+     */
+    public function __construct($message = null, $code = null, Exception $previous = null)
+    {
+        parent::__construct((string) $message, (int) $code, $previous);
+    }
+
 }
