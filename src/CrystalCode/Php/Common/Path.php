d944a1eb7f8f4c9469aa07f733dc7e6b6c48bd88
Dan Lugg <dan.lugg@gmail.com>
2019-01-05 13:55:06 -0500

    Add Path normalizer.

---
+++ src/CrystalCode/Php/Common/Path.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace CrystalCode\Php\Common;
+
+final class Path
+{
+
+    /**
+     * 
+     * @param string $path
+     * @param string $separator
+     * @return string
+     */
+    public static function normalize(string $path, string $separator = '/'): string
+    {
+        $segments = [];
+
+        foreach (preg_split('([\\\\/]+)', $path, -1, PREG_SPLIT_NO_EMPTY) as $segment) {
+            switch ($segment) {
+                case '.':
+                    break;
+                case '..':
+                    array_pop($segments);
+                    break;
+                default:
+                    array_push($segments, $segment);
+                    break;
+            }
+        }
+
+        return implode($separator, $segments);
+    }
+
+}
